a61d4066641dcb4696b91e949e175346ef089e92
Najeeb <sulaimannajeebadesoji@gmail.com>
2026-03-07 23:13:32 +0000

    Added data generation script

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ from datetime import datetime
 
 
 class Config(BaseModel):
-    NUM_PRODUCTS = 5000
-    NUM_STORES = 800
-    NUM_SUPPLIERS = 300
-    NUM_WAREHOUSES = 20
+    NUM_PRODUCTS: int = 5000
+    NUM_STORES: int = 800
+    NUM_SUPPLIERS: int = 300
+    NUM_WAREHOUSES: int = 20
 
-    DAYS = 7
+    DAYS: int = 7
 
-    MAX_DAILY_SALES = 200000
-    MAX_SHIPMENTS_PER_DAY = 50000
+    MAX_DAILY_SALES: int = 200000
+    MAX_SHIPMENTS_PER_DAY: int = 50000
 
-    START_DATE = datetime(2026, 3, 10)
+    START_DATE: datetime = datetime(2026, 3, 10)
 
-    DATA_DIR = "data"
+    DATA_DIR: str = "data"
